32ca85b8770e5b8bd1d2eeaec55168beb441ba40
siddiq57 <98172700+siddiq57@users.noreply.github.com>
2022-03-12 22:08:33 -0500

    Add files via upload

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,30 @@
+package midtermreviewcodeforpartc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ahmad
+ */
+public class PasswordValidator {
+
+    public boolean passwordValidator(String password) {
+
+        boolean validPassword;
+        int specialCharecter = 0;
+        //iterate over each character to see if it is a special character
+        for (int i = 0; i < password.length(); i++) {
+            if (!(Character.isLetterOrDigit(password.charAt(i)))) {
+                //now we know there is at least one special character
+                specialCharecter++;
+            }
+        }
+        if (specialCharecter > 0 && password.length() > 7) {
+            validPassword = true;
+        } else {
+            validPassword = false;
+        }
+        return validPassword;
+    }
+
+}
